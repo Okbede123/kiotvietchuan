8e8b3861bfde4389c8558ecc3f6d6cc2ea025c7a
duc.vm4 <vuminhduc24081997@gmail.com>
2023-01-30 14:12:22 +0700

    them dong thu 3 tu nhanhTest

--- src/main/java/org/example/Testcase/Test2.java
+++ src/main/java/org/example/Testcase/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test ");
         System.out.println("test tiep chinh sua tu nhanh duc bo sung");
+        System.out.println("them dong nua tu nhanhtest");
     }
 }
